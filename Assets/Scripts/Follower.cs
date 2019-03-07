6a52506e985735f4ce7a8ce78364da5d5341837b
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-03-07 15:21:39 -0700

    Follower System Added

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Cinemachine;
 
 public class Follower : MonoBehaviour
 {
     [SerializeField, Range(0f, 10f)]
     float moveSpeed = 0f;
+    [SerializeField] Transform targetFollower;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Player player;
 
+    [SerializeField] CinemachineVirtualCamera Follow;
     Animator anim;
 
     void Start()
@@ -17,6 +23,15 @@ public class Follower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(targetFollower.position);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            agent.enabled = !agent.enabled;
+
+            player.enabled = !agent.enabled;
+
+            Follow.enabled = !Follow.enabled;
+        }
     }
 }

92fe9a107ce671c4fa82ed3cd0e6af1a7a71ab0c
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-03-06 22:19:16 -0700

    Second Character

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)]
+    float moveSpeed = 0f;
+
+    Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
